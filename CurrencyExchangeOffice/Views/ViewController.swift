15d87a467d58b99646fab491cf18cd6a5f68fb72
Leszek Baca <leszek.baca@op.pl>
2024-10-04 12:12:06 +0200

    Displaing the number of currencies retrieved from API

--- CurrencyExchangeOffice/Views/ViewController.swift
+++ CurrencyExchangeOffice/Views/ViewController.swift
@@ -8,17 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-let urlString = "https://api.coinbase.com/v2/currencies"
     
-   // @IBOutlet weak var myLabel: UILabel!
+    let urlString = "https://api.coinbase.com/v2/currencies"
+    
+    // @IBOutlet weak var myLabel: UILabel!
     var myLabel:UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         myLabel = UILabel()
-        myLabel.text = "Hello"
+        //myLabel.text = "Hello"
         
         view.addSubview(myLabel)
         
@@ -29,15 +29,26 @@ let urlString = "https://api.coinbase.com/v2/currencies"
             .centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         myLabel.font = UIFont.systemFont(ofSize: 36)
         
+//        fetchData(from: urlString) { result in
+//            switch result {
+//            case .success(let data):
+//                print(data.data.count)
+//            case .failure(let error):
+//                print("Error: \(error.localizedDescription)")
+//            }
+//        }
+//        
         
         fetchData(from: urlString) { result in
-            switch result {
-            case .success(let data):
-                print(data.data.count)
-            case .failure(let error):
-                print("Error: \(error.localizedDescription)")
-            }
-        }
+                    DispatchQueue.main.async {
+                        switch result {
+                        case .success(let dataModel):
+                            self.myLabel.text = "Count: \(dataModel.data.count)"
+                        case .failure(let error):
+                            self.myLabel.text = "Error: \(error.localizedDescription)"
+                        }
+                    }
+                }
     }
 }
 
