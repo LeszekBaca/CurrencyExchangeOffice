bdbfa0bd3aad1fd6b9f34f1c21c20df8aaa89e74
Leszek Baca <leszek.baca@op.pl>
2024-10-04 13:45:08 +0200

    Displaing json on pockerView

--- CurrencyExchangeOffice/Views/ViewController.swift
+++ CurrencyExchangeOffice/Views/ViewController.swift
@@ -7,19 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     let urlString = "https://api.coinbase.com/v2/currencies"
-    
-    // @IBOutlet weak var myLabel: UILabel!
-    var myLabel:UILabel!
+    var dataModels: [DataModel] = []
+    var myLabel: UILabel!
+    var pickerView: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         myLabel = UILabel()
-        //myLabel.text = "Hello"
-        
         view.addSubview(myLabel)
         
         myLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -29,26 +27,48 @@ class ViewController: UIViewController {
             .centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         myLabel.font = UIFont.systemFont(ofSize: 36)
         
-        //        fetchData(from: urlString) { result in
-        //            switch result {
-        //            case .success(let data):
-        //                print(data.data.count)
-        //            case .failure(let error):
-        //                print("Error: \(error.localizedDescription)")
-        //            }
-        //        }
-        //
+        
+        // Initialization UIPickerView
+        pickerView = UIPickerView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pickerView)
+        
+        // Setting restrictions
+        pickerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        pickerView.topAnchor.constraint(equalTo: myLabel.bottomAnchor, constant: 20).isActive = true
+        pickerView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         
         fetchData(from: urlString) { result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let dataModel):
-                    self.myLabel.text = "Count: \(dataModel.data.count)"
+                    self.dataModels = dataModel.data
+                    self.pickerView.reloadAllComponents()
+                    self.myLabel.text = "Liczba walut: \(dataModel.data.count)"
                 case .failure(let error):
                     self.myLabel.text = "Error: \(error.localizedDescription)"
                 }
             }
         }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // Number of columns
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataModels.count // Number of currencies
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataModels[row].name // Display currency name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = dataModels[row]
+        myLabel.text = "ID: \(selectedCurrency.id), Min Size: \(selectedCurrency.min_size)"
     }
 }
 
