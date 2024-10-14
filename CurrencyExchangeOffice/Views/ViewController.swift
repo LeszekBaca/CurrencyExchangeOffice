b4d0019a075b523b20866b23b64beff62f2f7840
Leszek Baca <leszek.baca@op.pl>
2024-10-14 09:54:23 +0200

    first commit

--- CurrencyExchangeOffice/Views/ViewController.swift
+++ CurrencyExchangeOffice/Views/ViewController.swift
@@ -7,91 +7,97 @@
 import UIKit
 //class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 class ViewController: UIViewController {
-  //@IBOutlet var picker: UIPickerView!
-  weak var tableView: UITableView!
-  let urlString = "https://api.coinbase.com/v2/currencies"
-  var dataModels: [DataModel] = []
-  var myLabel: UILabel!
-  var pickerView: UIPickerView!
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-  }
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    myLabel = UILabel()
-    view.addSubview(myLabel)
-    myLabel.translatesAutoresizingMaskIntoConstraints = false
-    myLabel
-      .centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    myLabel
-      .centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-    myLabel.font = UIFont.systemFont(ofSize: 36)
-    //uitableview lub colectonview
-    //prezentacja nowego view controlera
-    //obsluga bledow sieciowych i nie tylko
-    //testy jednostkowy sprawdzajacy mapowanie jesona ()
-    //budowanie widoku z storyboard (autolayout uistackview uiimage)
-    // Initialization UIPickerView
-    //    pickerView = UIPickerView()
-    //    pickerView.delegate = self
-    //    pickerView.dataSource = self
-    //    pickerView.translatesAutoresizingMaskIntoConstraints = false
-    //    view.addSubview(pickerView)
-    // Setting restrictions
-    //    pickerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    //    pickerView.topAnchor.constraint(equalTo: myLabel.bottomAnchor, constant: 20).isActive = true
-    //    pickerView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-    fetchData(from: urlString) { result in
-      DispatchQueue.main.async {
-        switch result {
-        case .success(let dataModel):
-          self.dataModels = dataModel.data
-          // self.pickerView.reloadAllComponents()
-          self.myLabel.text = "Liczba walut: \(dataModel.data.count)"
-        case .failure(let error):
-          self.myLabel.text = "Error: \(error.localizedDescription)"
+    //@IBOutlet var picker: UIPickerView!
+    weak var tableView: UITableView!
+    let urlString = "https://api.coinbase.com/v2/currencies"
+    var dataModels: [DataModel] = []
+    var myLabel: UILabel!
+    var pickerView: UIPickerView!
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myLabel = UILabel()
+        view.addSubview(myLabel)
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        myLabel
+            .centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        myLabel
+            .centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        myLabel.font = UIFont.systemFont(ofSize: 36)
+        //uitableview lub colectonview
+        //prezentacja nowego view controlera
+        //obsluga bledow sieciowych i nie tylko
+        //testy jednostkowy sprawdzajacy mapowanie jesona ()
+        //budowanie widoku z storyboard (autolayout uistackview uiimage)
+        // Initialization UIPickerView
+        //    pickerView = UIPickerView()
+        //    pickerView.delegate = self
+        //    pickerView.dataSource = self
+        //    pickerView.translatesAutoresizingMaskIntoConstraints = false
+        //    view.addSubview(pickerView)
+        // Setting restrictions
+        //    pickerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //    pickerView.topAnchor.constraint(equalTo: myLabel.bottomAnchor, constant: 20).isActive = true
+        //    pickerView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        fetchData(from: urlString) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let dataModel):
+                    self.dataModels = dataModel.data
+                    // self.pickerView.reloadAllComponents()
+                    self.myLabel.text = "Liczba walut: \(dataModel.data.count)"
+                case .failure(let error):
+                    self.myLabel.text = "Error: \(error.localizedDescription)"
+                }
+            }
         }
-      }
     }
-  }
-  var ViewController: UITableViewDataSource {
-      
-    func tableView(_ tableView: UITableView, numberOfRowsInComponent component: Int) -> Int {
-      return dataModels.count // Number of currencies
+    
+    var ViewController: UITableViewDataSource {
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInComponent component: Int) -> Int {
+            return dataModels.count // Number of currencies
+        }
+        
+        func pickerView(_ tableView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return dataModels[row].name // Display currency name
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRow row: Int) -> UITableViewCell {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "testVC")
+            vc.navigationItem.title = dataModels[row].name
+            navigationController?.pushViewController(vc, animated: true)
+            
+            let selectedCurrency = dataModels[row]
+            //myLabel.text = "ID: \(selectedCurrency.id), Min Size: \(selectedCurrency.min_size)"
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+            cell.textLabel?.text = dataModels[row].name
+            //cell.selectionStyle = .none
+            return cell
+        }
+        return self.ViewController
     }
-      func tableView(_ tableView: UITableView, didSelectRow row: Int) -> UITableViewCell {
-      let storyboard = UIStoryboard(name: "Main", bundle: nil)
-      let vc = storyboard.instantiateViewController(withIdentifier: "testVC")
-      vc.navigationItem.title = dataModels[row].name
-      navigationController?.pushViewController(vc, animated: true)
-          
-      let selectedCurrency = dataModels[row]
-      //myLabel.text = "ID: \(selectedCurrency.id), Min Size: \(selectedCurrency.min_size)"
-      let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-      cell.textLabel?.text = dataModels[row].name
-      //cell.selectionStyle = .none
-      return cell
-    }
-      return self.ViewController
-  }
-  //  func numberOfComponents(in pickerView: UIPickerView) -> Int {
-  //    return 1 // Number of columns
-  //  }
-  //
-  //  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-  //    return dataModels.count // Number of currencies
-  //  }
-  //
-  //  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-  //    return dataModels[row].name // Display currency name
-  //  }
-  //  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-  //    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-  //    let vc = storyboard.instantiateViewController(withIdentifier: "testVC")
-  //    vc.navigationItem.title = dataModels[row].name
-  //    navigationController?.pushViewController(vc, animated: true)
-  //
-  //    let selectedCurrency = dataModels[row]
-  //    myLabel.text = "ID: \(selectedCurrency.id), Min Size: \(selectedCurrency.min_size)"
-  //  }
+    //  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    //    return 1 // Number of columns
+    //  }
+    //
+    //  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    //    return dataModels.count // Number of currencies
+    //  }
+    //
+    //  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    //    return dataModels[row].name // Display currency name
+    //  }
+    //  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    //    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    //    let vc = storyboard.instantiateViewController(withIdentifier: "testVC")
+    //    vc.navigationItem.title = dataModels[row].name
+    //    navigationController?.pushViewController(vc, animated: true)
+    //
+    //    let selectedCurrency = dataModels[row]
+    //    myLabel.text = "ID: \(selectedCurrency.id), Min Size: \(selectedCurrency.min_size)"
+    //  }
 }
